88809bd7b4894c79a461fb1f7f07d109706e83bd
camilo rios <camilo.rios1@mail.udp.cl>
2025-05-11 23:33:49 -0400

    update: validacion formulario actualizacion, params -> param

--- server/src/middleware/validaciones/asignaturas.validation.js
+++ server/src/middleware/validaciones/asignaturas.validation.js
@@ -1,4 +1,4 @@
-import { body, validationResult } from 'express-validator';
+import { param, body, validationResult } from 'express-validator';
 
 export const ValidarAsignatura = [
   body('codigo')
