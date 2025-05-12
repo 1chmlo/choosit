6e8b590da7e2b77a4397a29b51977ed70b29d17e
camilo rios <camilo.rios1@mail.udp.cl>
2025-05-11 23:49:16 -0400

    update: borrar libreria dotenv, no se usa en cliente

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,2 @@
-import dotenv from 'dotenv';
-
-// Config() considera como raiz la carpeta donde está el package.json
-dotenv.config({ path: './.env' });
 
 export const REACT_APP_BACKEND_URL = process.env.REACT_APP_BACKEND_URL || 'http://localhost:3000';
