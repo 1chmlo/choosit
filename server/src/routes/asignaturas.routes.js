272ff13b2428fb0573a8f5413cab7aba6c1ddf68
camilo rios <camilo.rios1@mail.udp.cl>
2025-05-13 16:21:09 -0400

    update: nombre funciones validar asignaturas

--- server/src/routes/asignaturas.routes.js
+++ server/src/routes/asignaturas.routes.js
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import { add_subject, modify_subject} from '../controllers/asignaturas.controller.js';
-import { ValidarAsignatura, Validarcambio } from '../middleware/validaciones/asignaturas.validation.js';
+import { ValidateCreateAsignatura, ValidateUpdateAsignatura } from '../middleware/validaciones/asignaturas.validation.js';
 
 const router = Router();
-router.post('/',ValidarAsignatura, add_subject);
-router.put('/:id',Validarcambio, modify_subject);
+router.post('/',ValidateCreateAsignatura, add_subject);
+router.put('/:id',ValidateUpdateAsignatura, modify_subject);
 
 export default router;
