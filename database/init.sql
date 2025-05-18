2ef7f647472dca4e2d831edf46cc2bff1158b1c7
camilo rios <camilo.rios1@mail.udp.cl>
2025-05-18 17:40:13 -0400

    update: initi.sql NOT NULL y valores por defecto

--- database/init.sql
+++ database/init.sql
@@ -4,41 +4,41 @@ CREATE EXTENSION IF NOT EXISTS unaccent;
 
 
 CREATE TABLE usuarios (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  nombre varchar(20),
-  apellido varchar(20),
-  username varchar(20) UNIQUE,
-  email varchar(100) UNIQUE,
-  contrasena varchar(100),
-  reputacion int,
-  activo bool,
-  verificado bool,
-  anio_ingreso int,
-  created_at TIMESTAMPTZ DEFAULT now()
+  id UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+  nombre varchar(20) NOT NULL,
+  apellido varchar(20) NOT NULL,
+  username varchar(20) UNIQUE NOT NULL,
+  email varchar(100) UNIQUE NOT NULL,
+  contrasena varchar(100) NOT NULL,
+  reputacion int NOT NULL DEFAULT 0,
+  activo bool NOT NULL DEFAULT true,
+  verificado bool NOT NULL DEFAULT false,
+  anio_ingreso int NOT NULL,
+  created_at TIMESTAMPTZ DEFAULT now() NOT NULL
 );
 
 CREATE TABLE asignaturas (
-  id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
-  codigo varchar(20) UNIQUE,
-  nombre varchar(50),
-  semestre int,
-  descripcion varchar(500),
-  n_encuestas int,
-  lab bool,
-  controles bool,
-  proyecto bool,
-  cfg bool
+  id UUID DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+  codigo varchar(20) UNIQUE NOT NULL,
+  nombre varchar(50) NOT NULL,
+  semestre int NOT NULL,
+  descripcion varchar(500) NOT NULL,
+  n_encuestas int NOT NULL DEFAULT 0,
+  lab bool NOT NULL,
+  controles bool NOT NULL,
+  proyecto bool NOT NULL,
+  cfg bool NOT NULL
 );
 
 CREATE TABLE tipo_pregunta (
-  id uuid PRIMARY KEY,
-  tipo varchar(50)
+  id uuid PRIMARY KEY NOT NULL,
+  tipo varchar(50) NOT NULL
 );
 
 CREATE TABLE preguntas (
-  id uuid PRIMARY KEY,
-  id_tipo uuid,
-  pregunta varchar(20),
+  id uuid PRIMARY KEY NOT NULL,
+  id_tipo uuid NOT NULL,
+  pregunta varchar(20) NOT NULL,
   CONSTRAINT FK_pregunta_id_tipo
     FOREIGN KEY (id_tipo)
       REFERENCES tipo_pregunta(id)
@@ -46,10 +46,9 @@ CREATE TABLE preguntas (
 );
 
 CREATE TABLE encuestas (
-  id uuid PRIMARY KEY,
-  id_profesor uuid,
-  id_usuario uuid,
-  id_asignatura uuid,
+  id uuid DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+  id_usuario uuid NOT NULL, 
+  id_asignatura uuid NOT NULL,
   CONSTRAINT FK_encuesta_id_usuario
     FOREIGN KEY (id_usuario)
       REFERENCES usuarios(id)
@@ -61,11 +60,11 @@ CREATE TABLE encuestas (
 );
 
 CREATE TABLE respuestas (
-  id uuid,
-  id_encuesta uuid,
-  id_pregunta uuid,
-  id_usuario uuid,
-  respuesta int,
+  id uuid DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+  id_encuesta uuid NOT NULL,
+  id_pregunta uuid NOT NULL,
+  id_usuario uuid NOT NULL,
+  respuesta int NOT NULL,
   CONSTRAINT FK_respuestas_id_pregunta
     FOREIGN KEY (id_pregunta)
       REFERENCES preguntas(id)
@@ -81,12 +80,12 @@ CREATE TABLE respuestas (
 );
 
 CREATE TABLE comentarios (
-  id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
-  id_usuario uuid,
-  id_asignatura uuid,
-  reputacion int,
-  fecha timestamp,
-  texto varchar(250),
+  id uuid DEFAULT gen_random_uuid() PRIMARY KEY NOT NULL,
+  id_usuario uuid NOT NULL,
+  id_asignatura uuid NOT NULL,
+  reputacion int NOT NULL DEFAULT 0,
+  fecha TIMESTAMPTZ DEFAULT now() NOT NULL,
+  texto varchar(250) NOT NULL,
   CONSTRAINT FK_comentario_id_usuario
     FOREIGN KEY (id_usuario)
       REFERENCES usuarios(id)
@@ -98,10 +97,10 @@ CREATE TABLE comentarios (
 );
 
 CREATE TABLE respuestas_ponderadas (
-  id uuid PRIMARY KEY,
-  id_asignatura uuid,
-  id_pregunta uuid,
-  respuesta_calculada int,
+  id uuid PRIMARY KEY NOT NULL,
+  id_asignatura uuid NOT NULL,
+  id_pregunta uuid NOT NULL,
+  respuesta_calculada int NOT NULL,
   CONSTRAINT FK_respuestas_ponderadas_id_asignatura
     FOREIGN KEY (id_asignatura)
       REFERENCES asignaturas(id)
