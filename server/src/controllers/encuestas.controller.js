232d4b7a876a1f078ca0b75efc67ab8ad89563b8
DiegoBan <bandadiego452@gmail.com>
2025-05-28 01:33:06 -0400

    creacion metodo POST nueva evaluación

--- server/src/controllers/encuestas.controller.js
+++ server/src/controllers/encuestas.controller.js
@@ -1,6 +1,38 @@
 import { pool } from '../db.js';
 import bcrypt, { compare } from 'bcrypt';
 
+export const create_encuesta = async (req, res) => {
+  try{
+    const id_user = req.userId;
+    const { id_asignatura, respuestas } = req.body;
+    const verificacion = await pool.query(`SELECT id FROM usuarios WHERE id = $1`, [id_user]);
+    if(verificacion.rowCount === 0) return res.status(404).json({ok: false, message: 'Usuario no existe'});
+    const { rows: preguntasEncuesta } = await pool.query(`
+      SELECT p.id
+      FROM preguntas p
+      JOIN encuestas e ON p.id_tipo_pregunta = e.id_tipo_pregunta
+      WHERE e.id_asignatura = $1
+      `, [id_asignatura]);
+    const preguntasDB = preguntasEncuesta.map((fila) => fila.id);
+    const preguntasUser = respuestas.map((r) => r.id_pregunta || "");
+    if(preguntasDB.length !== preguntasUser.length) return res.status(400).json({ok: false, message: 'Debe responder toda la encuesta'});
+    const faltantes = preguntasDB.filter(
+      (idPregunta) => !preguntasUser.includes(idPregunta)
+    );
+    if(faltantes.length > 0) return res.status(400).json({ok: false, message: 'Faltan preguntas de id: ' + faltantes.join(", ")});
+    for(const {id_pregunta, respuesta} of respuestas){
+      await pool.query(
+        `INSERT INTO evaluacion (id_pregunta, id_asignatura, id_usuario, respuesta)
+        VALUES ($1, $2, $3, $4)`, [id_pregunta, id_asignatura, id_user, respuesta]
+      );
+    }
+    return res.status(201).json({ok: true, message: 'Insertado correctamente'});
+  }catch(error){
+    console.error('Error al insertar:', error);
+    return res.status(500).json({ok: false, message: 'Error interno del servidor'})
+  }
+}
+
 export const delete_encuesta = async (req, res) => {
   try{
   const id = req.userId; //Obtiene el id del usuario para trabajar con el
